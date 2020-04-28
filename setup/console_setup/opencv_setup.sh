6ad5dccaea6085e7d7ce70c2271f24fe5eca703f
garrett <garrett.stuart.clem@gmail.com>
2020-04-28 20:42:08 +0100

    opencv working on pi3 - added pip3 install scripts

---
+++ setup/console_setup/opencv_setup.sh
@@ -0,0 +1,14 @@
+
+#Issues running the latest version of opencv through pip install on pi.
+#A github issue posted this solution
+#https://github.com/EdjeElectronics/TensorFlow-Object-Detection-on-the-Raspberry-Pi/issues/67#issuecomment-557679983
+pip3 install opencv-python==3.4.6.27
+
+#These may be necessary, unknown at this point
+#sudo apt-get install libcblas-dev
+#sudo apt-get install libhdf5-dev
+#sudo apt-get install libhdf5-serial-dev
+#sudo apt-get install libatlas-base-dev
+#sudo apt-get install libjasper-dev 
+#sudo apt-get install libqtgui4 
+#sudo apt-get install libqt4-test
