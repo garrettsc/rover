7cd6a3dbc413357556ff6a01444bc66bed9b67c5
garrett <garrett.stuart.clem@gmail.com>
2020-04-29 00:30:38 +0100

    organizing

---
+++ tests/motor_tests/motor_test.py
@@ -0,0 +1,17 @@
+from gpiozero import Motor
+from time import sleep
+
+motor_1 = Motor(forward=26, backward=19)
+motor_2 = Motor(forward=13, backward=20)
+
+for i in range(2):
+    motor_1.forward()
+    motor_2.forward()
+    sleep(5)
+    motor_1.backward()
+    motor_2.backward()
+    sleep(5)
+
+motor_1.stop()
+motor_1.stop()
+
